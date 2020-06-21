39d4205a04eb0bdd7b9758d97bafd14c58479bf9
Anfesal <61337242+Anfesal@users.noreply.github.com>
2020-06-21 18:46:42 -0500

    Se adiciona funcionalidad de mapa

--- categorias.php
+++ categorias.php
@@ -22,6 +22,21 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?sensor=true" async defer></script>
+    <script type="text/javascript" src="codigo.js"></script>
+
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
+    <style type="text/css">
+        #mapa {
+            width: 100%;
+            height: 180%;
+        }
+         footer{
+            background-color: rgb(192, 189, 189);
+            color: black;
+         }
+        
+    </style>
 </head>
 
 <body>
@@ -412,11 +427,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12 col-md-6 text-left">
-                    <h6 class="text-muted lead">CONTACTO:</h6>
-                    <h6 class="text-muted">
-                        Calle 65 #55-30<br> Barrio Chagualo.<br> Medellín, Antioquia. <br> Teléfono: 3182044125.<br>
-                    </h6>
+               
+                    <div id="mapa"></div>
+                    <div id="ubicacion"></div>
+                
                 </div>
+
+               
 
                 <div class="col-xs-12 col-md-6 text-right">
                     <h6 class="text-muted lead">ENCUENTRANOS EN LAS REDES</h6>
