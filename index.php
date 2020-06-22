3caa7a9d9ddb916565a2a6fd444fd8a94fd7505a
robinson715 <robinson.jaramillo11@gmail.com>
2020-06-22 13:07:14 -0500

    mejora a barra de menu

--- index.php
+++ index.php
@@ -34,12 +34,13 @@ window.onscroll = function(){
 </head>
 
 <body>
-    <nav id="nav-a" class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <nav id="nav-a" class="navbar fixed-top navbar-expand-lg navbar-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="index.php"><img src="img/log2.PNG" alt="" width="113" height="64"></a>
+        <a class="navbar-brand" href="index.php"></a>
+        <div id="ensayo" class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                
                 <li class="nav-item ">
